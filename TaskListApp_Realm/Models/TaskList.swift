ccdaead18290598a741c86d81220a6e8ad2f2fdf
auhustsinovich <avgustinovich4@gmail.com>
2022-08-08 15:39:04 +0200

    Initial Commit

---
+++ TaskListApp_Realm/Models/TaskList.swift
@@ -0,0 +1,23 @@
+//
+//  TaskList.swift
+//  testRealmApp
+//
+//  Created by Daniil Auhustsinovich on 8.08.22.
+//
+
+
+import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
